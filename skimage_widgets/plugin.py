87e368b82f69d3cee1184c73aca21e8aeab53c6b
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-01-26 09:52:20 -0800

    use functional hookimpl

--- skimage_widgets/plugin.py
+++ skimage_widgets/plugin.py
@@ -2,12 +2,8 @@ from napari_plugin_engine import napari_hook_implementation
 
 
 @napari_hook_implementation
-def napari_experimental_provide_dock_widget():
+def napari_experimental_provide_function_widget():
     from .annotate import annotate_module
     from skimage import filters
-    from magicgui import magic_factory
 
-    return [
-        magic_factory(f, layout="vertical", auto_call=True)
-        for f in annotate_module(filters).values()
-    ]
+    return list(annotate_module(filters).values())
